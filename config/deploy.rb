faac381b5027d02d573e4bbdfb5fa152a934e422
swytman <swytman@gmail.com>
2014-06-29 15:50:36 +0400

    remove index from public

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,7 +44,7 @@ namespace :unicorn do
   task :restart do
     on roles :all do
       #execute "if [ -f #{fetch(:unicorn_pid)} ]; then disown `cat #{fetch(:unicorn_pid)} && kill -QUIT `cat #{fetch(:unicorn_pid)}`; fi"
-      execute "if [ -f #{fetch(:unicorn_pid)} && ps -p `cat #{fetch(:unicorn_pid)}` ]; then disown `cat #{fetch(:unicorn_pid)}` && kill -USR2 `cat #{fetch(:unicorn_pid)}`; else cd #{fetch(:current_path)} && bundle exec unicorn -c #{fetch(:unicorn_conf)} -E #{fetch(:stage)} -D; fi"
+      execute "if [ -f #{fetch(:unicorn_pid)} ]; then kill -13 `cat #{fetch(:unicorn_pid)}`; else cd #{fetch(:current_path)} && bundle exec unicorn -c #{fetch(:unicorn_conf)} -E #{fetch(:stage)} -D; fi"
     end
   end
 
