6b50fcbb36ed6d63396d5868e78a9fd187cbe017
swytman <swytman@gmail.com>
2014-06-29 16:56:53 +0400

    unic

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -4,13 +4,11 @@ pid_file = "#{deploy_to}/shared/pids/unicorn.pid"
 socket_file= "#{deploy_to}/shared/unicorn.sock"
 log_file = "#{deploy_to}/shared/log/unicorn.log"
 err_log = "#{deploy_to}/shared/log/unicorn_error.log"
-old_pid = pid_file + '.oldbin'
 
 timeout 30
 worker_processes 2 # increase or decrease
 working_directory app_root
-listen 8080, :tcp_nopush => true
-#listen socket_file, :backlog => 1024
+listen socket_file, :backlog => 1024
 
 pid pid_file
 stderr_path err_log
